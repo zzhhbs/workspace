3e97a6d2fd499e2826e7dbd7d110414c25cfbdfe
zzhhbs <908924736@qq.com>
2020-08-08 15:28:12 +0800

    第一次测试

---
+++ HelloGit/src/main/java/cn/kgc/hello/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.kgc.hello;
+
+/**
+ * Created by helloworld on 2020/8/8.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+    }
+}
